0e8a5c4b97ad96b8af7a50f67ce7aa4701bfb718
Alljuly <ajsc1@aluno.ifal.edu.br>
2021-08-20 01:21:09 +0000

    A SOMA DEU CERTO MEU DEUS

---
+++ calc-api/src/main/java/app/Soma.java
@@ -0,0 +1,22 @@
+package app;
+
+import io.jooby.annotations.*;
+import io.jooby.exception.BadRequestException;
+public class Soma{
+    
+    @Path("/soma/{op}/{po}")
+    @GET
+    public String rotaSoma(@PathParam ("op") String op, @PathParam ("po") String po){
+   
+       try{  
+           //converter em double
+            double sum = Double.parseDouble(op) + Double.parseDouble(po);
+            // retornar soma dos parametros 
+            return String.format("Soma:%s", sum);
+        } catch (NumberFormatException nfe) {
+            new BadRequestException (String.format("Formato inválido"));
+        }   
+        return "";
+    }
+
+}
